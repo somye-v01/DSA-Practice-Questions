8e8532830105647e11e7483caa64ac3affc759dd
Somyeh Virmani <93946642+somye-v01@users.noreply.github.com>
2023-09-02 22:46:06 +0530

    Time: 7 ms (14.67%) | Memory: 11.6 MB (5.41%) - LeetSync

---
+++ arrays/2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        int ans = s.find(part);
+        //BaseCase
+        if(ans!=string::npos){
+        //ek case solve kr do
+        string left_str = s.substr(0,ans);
+        string right_str = s.substr(ans+part.length(),s.length());
+        s = left_str + right_str;
+        return removeOccurrences(s,part);
+        }
+        else{
+            return s;
+        }
+        
+    }
+};
