ef340db829d35627b67fe3c513ab4e727d01f49d
Somyeh Virmani <93946642+somye-v01@users.noreply.github.com>
2023-09-05 10:33:39 +0530

    Time: 19 ms (93.69%) | Memory: 25.9 MB (71.11%) - LeetSync

--- arrays/1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ arrays/1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -1,42 +1,22 @@
-// class Solution {
-// public:
-//     vector<int> sortedSquares(vector<int>& nums) {
-//         map<int,int> m;
-//         vector<int> ans;
-//         for(int i = 0; i < nums.size(); i++){
-//             m[nums[i] * nums[i]]++;
-//         }
-//         for (auto& x : m){
-//             if (x.second == 1){
-//             ans.push_back(x.first);
-//             }
-//             while(x.second != 1 && x.second > 0){
-//                 ans.push_back(x.first);
-//                 --x.second;
-//             }
-//         }
-//         return ans;
-//     }
-// };
-
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        map<int, int> m;
-        vector<int> ans;
-
-        for (int num : nums) {
-            int squared = num * num;
-            m[squared]++;
+        int i = 0;
+        int j = nums.size() - 1 ;
+        vector<int>ans(nums.size());
+        int k = j;
+        while (k != -1){
+        if ((abs(nums[i])) > nums[j]){
+            ans[k] = (nums[i] * nums[i]);
+            i++;
+            k--;
         }
-
-        
-        for (auto& x : m) {
-            for (int i = 0; i < x.second; i++) {
-                ans.push_back(x.first);
+        else{
+            ans[k] = (nums[j] * nums[j]);
+            j--;
+            k--;
             }
         }
-
-        return ans;
+    return ans;
     }
 };
