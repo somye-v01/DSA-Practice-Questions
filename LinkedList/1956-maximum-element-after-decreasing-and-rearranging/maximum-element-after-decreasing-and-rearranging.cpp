e48ed9fddb00001ca8dfedfa673248f9c4832295
Somyeh Virmani <93946642+somye-v01@users.noreply.github.com>
2023-11-15 08:00:50 +0530

    Time: 63 ms (98.13%) | Memory: 51.5 MB (56.72%) - LeetSync

--- LinkedList/1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.cpp
+++ LinkedList/1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.cpp
@@ -15,7 +15,6 @@ public:
     
         for(int i = 1; i < arr.size(); i++){
             if(arr[i] - arr[i-1] > 1){
-                // ref = i;
                 arr[i] = ++arr[i-1];
             }
         }
