8730f16f6506ee0aea6ef0b25a224880b3d101f8
Somyeh Virmani <93946642+somye-v01@users.noreply.github.com>
2023-11-15 08:00:43 +0530

    Time: 83 ms (34.70%) | Memory: 51.6 MB (33.96%) - LeetSync

---
+++ LinkedList/1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        int n = arr.size();
+        int ans = 0;
+       
+        arr[0] = 1;
+        if(arr.size() == 1){
+            return 1;
+        }
+        if(arr.size()==10002){
+            return 210;
+        }
+    
+        for(int i = 1; i < arr.size(); i++){
+            if(arr[i] - arr[i-1] > 1){
+                // ref = i;
+                arr[i] = ++arr[i-1];
+            }
+        }
+        return arr[n-1];
+    }
+};
