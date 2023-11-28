06f9be0c6894b676a35834f4589e5d3ae5605da0
Somyeh Virmani <93946642+somye-v01@users.noreply.github.com>
2023-11-29 02:51:25 +0530

    Time: 3 ms (12.27%) | Memory: 6.5 MB (56.26%) - LeetSync

--- LinkedList/70-climbing-stairs/climbing-stairs.cpp
+++ LinkedList/70-climbing-stairs/climbing-stairs.cpp
@@ -3,7 +3,7 @@ public:
 
     int topDownDP(vector<int>& dp,int n){
         if(n==1) return 1;
-        if(n==0) return 0;
+        // if(n==0) return 0;
         if(n==2) return 2;
         if(dp[n]!=-1) return dp[n];
         dp[n] = topDownDP(dp,n-1) + topDownDP(dp,n-2);
