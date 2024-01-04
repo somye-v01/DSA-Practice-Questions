13699d07778b802f0bca4f640b5ea6f7e2cf8579
Somyeh Virmani <93946642+somye-v01@users.noreply.github.com>
2024-01-05 01:16:18 +0530

    Time: 83 ms (75.37%) | Memory: 8.7 MB (13.66%) - LeetSync

--- LinkedList/375-guess-number-higher-or-lower-ii/guess-number-higher-or-lower-ii.cpp
+++ LinkedList/375-guess-number-higher-or-lower-ii/guess-number-higher-or-lower-ii.cpp
@@ -18,10 +18,28 @@ public:
         return dp[start][end];
     }
 
+    int solveTab(int n){
+        vector<vector<int>>dp(n+2,vector<int>(n+2,0));
+        // dp[1]=1;
+        for(int start = n; start>=1; start--){
+            for(int end=1;end<=n;end++){
+                if(start>=end) continue;
+                else{
+                int ans = INT_MAX;
+                for(int i = start; i <= end; i++){
+                 ans = min(ans,i+max(dp[start][i-1],dp[i+1][end]));
+                }
+            dp[start][end] = ans;
+                }
+            }
+        }
+    return dp[1][n];
+    }
+
     int getMoneyAmount(int n) {
         //ans = i+max(solve(1,i),solve(i+1,n))
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        int ans =  solve(0,n,dp);
+        // vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        int ans =  solveTab(n);
         return ans;
     }
 };
