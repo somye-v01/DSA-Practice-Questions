dd79d625b7a371d184da8fd0a9f9aa665a5d6a5b
Somyeh Virmani <93946642+somye-v01@users.noreply.github.com>
2024-01-06 02:47:09 +0530

    Time: 188 ms (33.45%) | Memory: 10.3 MB (25.83%) - LeetSync

---
+++ LinkedList/279-perfect-squares/perfect-squares.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int numSquaresHelper(int n,vector<int>&dp){
+        //Base Case
+        // int i = 1;
+        if(n==0){
+            return 0;
+        }
+        if(n<0){
+            return 0;
+        }
+        if(dp[n]!=-1) return dp[n];
+       int ans= INT_MAX;
+        for(int i = 1; i <= sqrt(n); i++){
+            int perfectsquare = i*i;
+            ans = min(ans,1 + numSquaresHelper(n-perfectsquare,dp));
+        }
+        dp[n] = ans;
+        return dp[n];
+        
+    }
+    
+    int numSquares(int n) {
+        vector<int>dp(n+1,-1);
+        int ans = numSquaresHelper(n,dp);
+        return ans;
+    }
+};
