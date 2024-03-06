576fe6b8cee949df7e4650456bc1a7be80f36b69
Somyeh Virmani <93946642+somye-v01@users.noreply.github.com>
2024-03-06 12:55:41 +0530

    Time: 22 ms (7.15%) | Memory: 13.7 MB (8.96%) - LeetSync

---
+++ LinkedList/141-linked-list-cycle/linked-list-cycle.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        int i = 0;
+        map<ListNode*,bool>m;
+        while(head!=nullptr){
+            if(m[head]==true){
+                cout << "the duplicate im getting is: "<<head->val<<endl;
+                return true;
+            }
+            m[head]=true;
+            head=head->next;
+        }
+        return false;
+    }
+};
