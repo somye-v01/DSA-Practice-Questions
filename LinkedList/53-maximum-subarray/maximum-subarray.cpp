0e338fa780c0d77fca72928c836021c551b579b3
Somyeh Virmani <93946642+somye-v01@users.noreply.github.com>
2024-03-13 20:24:44 +0530

    Time: 78 ms (60.80%) | Memory: 70.2 MB (60.06%) - LeetSync

--- LinkedList/53-maximum-subarray/maximum-subarray.cpp
+++ LinkedList/53-maximum-subarray/maximum-subarray.cpp
@@ -1,32 +1,13 @@
 class Solution {
-private:
-    int maxSubArray(vector<int>& nums, int low, int high){
-        if(low == high){
-            return nums[low];
-        }
-        int leftMaxSumSubarray = INT_MIN, rightMaxSumSubarray = INT_MIN;
-
-        int mid = (low + high) >> 1;
-        int currSum = 0;
-        for(int i=mid; i>=low; i--){
-            currSum += nums[i];
-            leftMaxSumSubarray = max(leftMaxSumSubarray, currSum);
-        }
-
-        currSum = 0;
-        for(int i=mid+1; i<=high; i++){
-            currSum += nums[i];
-            rightMaxSumSubarray = max(rightMaxSumSubarray, currSum);
-        }
-
-        int maxSumSubarray =  leftMaxSumSubarray +  rightMaxSumSubarray;
-        int leftAnsSum = maxSubArray(nums, low, mid);
-        int rightAnsSum = maxSubArray(nums, mid+1, high);
-
-        return max(maxSumSubarray, max(leftAnsSum, rightAnsSum));
-    }
 public:
     int maxSubArray(vector<int>& nums) {
-        return maxSubArray(nums, 0, nums.size()-1);
+        int sum = 0;
+        int maxi = INT_MIN;
+        for(auto num : nums){
+            sum+=num;
+            if(sum>maxi) maxi=sum;
+            if(sum<0) sum = 0;
+        }
+        return maxi;
     }
 };
