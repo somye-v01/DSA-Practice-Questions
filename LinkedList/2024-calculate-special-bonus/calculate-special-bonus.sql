a996ba722f2356905f3445c465a9aaa41a03b17b
Somyeh Virmani <93946642+somye-v01@users.noreply.github.com>
2024-03-31 02:15:23 +0530

    Time: 1315 ms (30.09%) | Memory: 0B (100.00%) - LeetSync

--- LinkedList/2024-calculate-special-bonus/calculate-special-bonus.sql
+++ LinkedList/2024-calculate-special-bonus/calculate-special-bonus.sql
@@ -1,10 +1,7 @@
-/* Write your PL/SQL query statement below */
-SELECT employee_id,
-case WHEN MOD(employee_id,2) = 1 AND name NOT LIKE 'M%' THEN salary
-ELSE 0
-END
-AS "bonus"
-FROM Employees
-ORDER BY employee_id;
-
-
+# Write your MySQL query statement below
+SELECT employee_id, (
+    CASE
+    WHEN MOD(employee_id,2)=1 AND name NOT LIKE "M%" THEN salary
+    ELSE 0
+    END
+) AS bonus from Employees ORDER BY employee_id;
