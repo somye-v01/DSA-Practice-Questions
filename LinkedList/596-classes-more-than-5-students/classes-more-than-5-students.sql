936e26db7533c54704a1aefdf490bc13b5e92a5b
Somyeh Virmani <93946642+somye-v01@users.noreply.github.com>
2024-03-31 23:03:36 +0530

    Time: 557 ms (60.04%) | Memory: 0B (100.00%) - LeetSync

--- LinkedList/596-classes-more-than-5-students/classes-more-than-5-students.sql
+++ LinkedList/596-classes-more-than-5-students/classes-more-than-5-students.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
 SELECT class FROM Courses
 GROUP BY class
-HAVING count(student)>=5
-ORDER BY student DESC;
+HAVING count(student)>=5;
+-- ORDER BY student DESC;
