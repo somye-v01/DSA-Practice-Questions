b3b2e3da23952a887a0cbce2b266910f0ee3391c
Somyeh Virmani <93946642+somye-v01@users.noreply.github.com>
2024-04-16 18:18:23 +0530

    Time: 2 ms (42.26%) | Memory: 10 MB (17.62%) - LeetSync

--- LinkedList/94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ LinkedList/94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -14,9 +14,22 @@ public:
 vector<int> ans;
     vector<int> inorderTraversal(TreeNode* root) {
         if(!root) return ans;
-        inorderTraversal(root->left);
-        ans.push_back(root->val);
-        inorderTraversal(root->right);
+       // L N R
+       //jab tak left ja skte ho, left jao. put all nodes in a stack. jese hi nahi, ja skte, put that node in ans, and uska right bhi stack me daal do.
+       TreeNode* temp = root;
+       stack<TreeNode*>st;
+    //    st.push(root);
+       while(temp || !st.empty()){
+            while(temp){
+                st.push(temp);
+                temp = temp->left;
+            }
+            temp = st.top();
+            st.pop();
+            ans.push_back(temp->val);
+            // st.pop();
+            temp = temp->right;
+        }
         return ans;
     }
 };
